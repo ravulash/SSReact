2fad0a09592ca8d0e68dac119857ead9f35d8b6c
Shilpa <rshilpareddy@gmail.com>
2018-07-04 10:33:43 +0000

    changes

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -20,22 +20,49 @@ class App extends Component {
             <input type="text" name="exp"/>&nbsp;&nbsp;
             <button type="submit" form="form" value="Search">Search</button>
         </form>
+        <br/>
+        
         <div>
-          <table border="1">   
-            <thead>
-              <th>
-                <td>JobDetails</td>
-                <td>Description</td>
-                <td>Location</td>
-                <td>Open Date</td>
-              </th>
-            </thead>
+          <table border="1" align="center" >   
+            
+              <tr bgcolor="	#DDA0DD">
+                <th>JobDetails</th>
+                <th>Description</th>
+                <th>Location</th>
+                <th>Open Date</th>
+              </tr>
+              <tr bgcolor="#FFE4B5">
+                <td>QA LEAD</td>
+                <td>Lead 20 memebr team of ITO Automation</td>
+                <td>Hyderabad</td>
+                <td>June 10 2018</td>
+                </tr>
+                <tr bgcolor="#FFE4B5">
+                <td>QA Manager</td>
+                <td>Manage 40 memebr team of ITO Automation</td>
+                <td>Chennai</td>
+                <td>June 12 2018</td>
+                </tr>
+                <tr bgcolor="#FFE4B5">
+                <td>Analyst</td>
+                <td>software testing</td>
+                <td>Chennai</td>
+                <td>June 12 2018</td>
+                </tr>
+                <tr bgcolor="#FFE4B5">
+                <td>Senior Manager</td>
+                <td>Should able to manager 100 people team</td>
+                <td>Chennai</td>
+                <td>June 12 2018</td>
+                </tr>
          </table>
         </div>
+       
         <footer>
           <h6>Powered By SimpleSamosa</h6>
           </footer>
       </div>
+      
       
     );
   }
